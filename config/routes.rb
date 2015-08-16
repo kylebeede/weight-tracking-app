b63fb2c9775e00e66002ca3a6575ff1c89004bf6
Kyle Beede <kyle.beede@faithlife.com>
2015-08-15 23:16:19 -0700

    Add sign in/out functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   root 'static_pages#home'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
