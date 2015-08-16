792a6c6495b7560530bb2a1ee0268ebf56dd0968
Kyle Beede <kyle.beede@faithlife.com>
2015-08-15 18:37:19 -0700

    Add sign up functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def hello
-  	render text: "hello, world!"
-  end
+  before_filter :initialize_user
+  def initialize_user
+	  @user = User.new
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
