f88e90a0f9bff799c179f2f2a5aa086ca8a729b8
Kyle Beede <kyle.beede@faithlife.com>
2015-09-23 21:10:02 -0700

    Moved nav event listener to header layout

--- app/assets/javascripts/header.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	var navLink = document.querySelector( ".header-account-menu .header-nav-link" );
-	var subNav = document.querySelector( ".header-account-menu .sub-nav" );
-
-	navLink.addEventListener( "click", function(){
-		$(subNav).toggleClass("active");
-	} );
-});
