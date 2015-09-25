21710dd796817f0619f311ca24edb48d20838d02
Kyle Beede <kyle.beede@faithlife.com>
2015-09-24 22:06:23 -0700

    Add password reset

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,9 +7,12 @@ class UserMailer < ApplicationMailer
   #
   def account_activation(user)
     @user = user
-    @greeting = "Hi"
-
     mail to: user.email, subject: "WeightTracker account activation"
+  end
+
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "WeightTracker password reset"
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -17,9 +20,9 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.password_reset.subject
   #
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "WeightTracker password reset"
   end
+  
 end
